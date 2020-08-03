06aa086dcbd65df7d5afbfaf0f7f9111a9004e44
Tanish <Tanishkumar5007@gmail.com>
2020-08-04 02:57:22 +0530

    Corrected the code

--- all_checks.py
+++ all_checks.py
@@ -14,21 +14,16 @@ def check_disk_usage(disk, min_absolute, min_percent):
 	# Calculate how many free gigabytes
 	gigabytes_free = du.free/2**30
 	if percent_free < min_percent or  gigabytes_free < min_absolute:
-		return False
-	return True
-
-if not check_disk_usage("/", 2, 10):
-	print("Error: Not Enough disk space.")
-	sys.exit(1)
-
-    
+		return True
+	return False
 
 def main():
     if check_reboot():
         print("Pending reboot.")
         sys.exit(1)
-    else:
-        print("everything is OK.")
+    if check_disk_usage("/", 2, 10):
+    	print("Error: Not Enough disk space.")
+    	sys.exit(0)
 
 
 main()
