b8099698a108b9e7aa3ae2df805e69097ee82050
Tanish <Tanishkumar5007@gmail.com>
2020-08-04 14:47:13 +0530

    Fixed check_disk_usage condition to check the new order and the new variable name.

--- all_checks.py
+++ all_checks.py
@@ -13,17 +13,17 @@ def check_disk_usage(disk, min_gb, min_percent):
 	percent_free = 100 * du.free/du.total
 	# Calculate how many free gigabytes
 	gigabytes_free = du.free/2**30
-	if percent_free < min_percent or  gigabytes_free < min_gb:
+	if gigabytes_free < min_gb or percent_free < min_percent:
 		return True
 	return False
 
 def main():
     if check_reboot():
         print("Pending reboot.")
-        sys.exit(1)
+            sys.exit(1)
     if check_disk_usage(disk = "/", min_gb = 2, min_percent = 10):
-    	print("Disk full.")
-    	sys.exit(1)
+        print("Disk full.")
+        sys.exit(1)
 
     print("Everything OK.")
     sys.exit(0)
